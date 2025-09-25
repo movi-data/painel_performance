f429e7d364bd3b97d38dfaccd57ca904b3b7ba1c
movi-data <analistamovida@gmail.com>
2025-09-25 08:16:27 -0300

    Add files via upload

--- painel_performance.py
+++ painel_performance.py
@@ -3,24 +3,30 @@ import pandas as pd
 # --- ETAPA 1: Carregar o arquivo ---
 df_propostas = pd.read_csv("propostas.csv", sep=";", encoding="utf-8", low_memory=False)
 
-# --- ETAPA 2: Preparar Colunas ---
+df_canal = df_propostas.copy()
+
+# --- ETAPA 2: Transforar Colunas ---
 
 # Tranformar 'Canal' em maiuscula
-df_propostas['Canal'] = df_propostas['Canal'].str.upper()
+df_canal['Canal'] = df_canal['Canal'].str.upper()
 # Filtrar o primeiro Canal na coluna 'Canal'
-df_propostas['Canal'] = df_propostas['Canal'].str.split(',').str[0]
+df_canal['Canal'] = df_canal['Canal'].str.split(',').str[0]
 
 # Trocar na coluna 'Origem' onde for 'MobiBot' por 'Internet'
-df_propostas['Origem'] = df_propostas['Origem'].replace('MobiBot', 'Internet')
+df_canal['Origem'] = df_canal['Origem'].replace('MobiBot', 'Internet')
 
-# Filtrar leads apenas com 'Internet' em 'Origem'
-df_propostas = df_propostas[df_propostas['Origem'] == 'Internet']
+# Trocar na coluna 'Origem' onde for 'MobiBot' por 'Internet'
+df_canal['Origem'] = df_canal['Origem'].replace('MobiBot', 'Internet')
+
+# Renomear coluna 'qualificado'
+df_canal.rename(columns={'qualificado': 'Qualificado'}, inplace=True)
 
 # --- ETAPA 3: Fazer agrupamento ---
 
-# Agrupar leads por mês na coluna 'Data' e 'Canal'
-leads_por_canal_mes = df_propostas.groupby([pd.to_datetime(df_propostas["Data"], format="%d/%m/%Y", errors='coerce').dt.to_period("d"),
-                                            "Canal"]).size().reset_index(name="Quantidade de Leads")
+# Agrupar leads por mês na coluna 'Data' , 'Canal' e 'Qualificado'
+
+leads_por_canal_mes = df_canal.groupby([pd.to_datetime(df_canal["Data"], format="%d/%m/%Y", errors='coerce').dt.to_period("d"),
+                                            "Canal","Qualificado"]).size().reset_index(name="Quantidade de Leads")
 
 # --- ETAPA 4: Criar arquivo csv e download ---
 
@@ -28,4 +34,4 @@ leads_por_canal_mes = df_propostas.groupby([pd.to_datetime(df_propostas["Data"],
 leads_por_canal_mes.to_csv("leads_por_canal_mes.csv",sep=";" ,index=False)
 
 # Visualização para conferência no console
-print(leads_por_canal_mes.head())
+print(leads_por_canal_mes.tail())
